00e49f056153d42566d9f4dee15831da8e526b5e
Tyr Johanson <tyr@ca.com>
2018-09-13 21:17:37 +0000

    added time to market tooltip

--- cycle-time-data-app/src/javascript/utils/TsConstants.js
+++ cycle-time-data-app/src/javascript/utils/TsConstants.js
@@ -1,8 +1,6 @@
 Ext.define('TsConstants', {
     statics: {
         LABELS: {
-            BLOCKED: 'Blocked',
-            READY: 'Ready',
             TIME_TO_MARKET: 'Time To Market',
             IN_BLOCKED: 'In Blocked',
             IN_READY: 'In Ready'
