68110ed3966cf76653a3ad5461d67b4b99fd29c1
Tyr Johanson <tyr@ca.com>
2018-09-13 17:46:30 +0000

    time to market columns

---
+++ cycle-time-data-app/src/javascript/utils/TsConstants.js
@@ -0,0 +1,11 @@
+Ext.define('TsConstants', {
+    statics: {
+        LABELS: {
+            BLOCKED: 'Blocked',
+            READY: 'Ready',
+            TIME_TO_MARKET: 'Time To Market',
+            IN_BLOCKED: 'In Blocked',
+            IN_READY: 'In Ready'
+        }    
+    }
+});
